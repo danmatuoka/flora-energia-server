0bee408af148ff47d4a1769f21a8d8bda6ee94df
danmatuoka <dan_mtk@hotmail.com>
2024-12-21 19:19:54 -0300

    feat: implement get user profile

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,8 +2,11 @@ import { FastifyInstance } from 'fastify';
 
 import { signUp } from './sign-up';
 import { signIn } from './sign-in';
+import { verifyJwt } from '@/http/middlewares/verify-jwt';
+import { profile } from './profile';
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/auth/signup', signUp);
   app.post('/auth/signin', signIn);
+  app.get('/user/me', { onRequest: [verifyJwt] }, profile);
 }
