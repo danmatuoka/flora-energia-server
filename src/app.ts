73a50df4949d7f5a5d41a2fb4b8af8046f07edad
danmatuoka <dan_mtk@hotmail.com>
2024-12-23 18:37:17 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,17 @@ import fastifyJwt from '@fastify/jwt';
 import fastify from 'fastify';
 import { ZodError } from 'zod';
 import { env } from '@/env';
+import cors from '@fastify/cors';
 import { usersRoutes } from './http/controllers/users/routes';
 import { wordsRoutes } from './http/controllers/words/routes';
 
 export const app = fastify();
+
+app.register(cors, {
+  origin: '*', // Permite todas as origens. Substitua '*' por um URL específico para restringir a origem
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+});
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
